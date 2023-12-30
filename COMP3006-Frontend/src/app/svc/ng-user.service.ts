518247eaca78b89f24a369f5842ef2508075f4db
JamieTremaine <jamie.tremaine@students/plymouth.ac.uk>
2023-12-30 22:05:37 +0000

    user login

--- COMP3006-Frontend/src/app/svc/ng-user.service.ts
+++ COMP3006-Frontend/src/app/svc/ng-user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, lastValueFrom } from 'rxjs';
+import { UserService } from '../api/services';
+import { User, Userlogin } from '../api/models';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +9,29 @@ import { Subject } from 'rxjs';
 export class NgUserService {
 
   private loggedIn: boolean = false;
+  private user?: User;
   public LoggedInSubject = new Subject<boolean>();
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   public isLoggedIn(): boolean {
     return this.loggedIn;
   }
 
-  login(username: string, password: string): Promise<boolean> {
-    this.loggedIn = true;
-    this.LoggedInSubject.next(true);
-    return Promise.resolve(true);
-  }
+    login(username: string, password: string): Promise<boolean> {
+
+        const loginCreds: Userlogin = { username: username , password: password };
+
+        return lastValueFrom(this.userService.userLoginPost({body: loginCreds})).then((result) => {
+            if(result._id) {
+                this.user = result;
+                this.loggedIn = true;
+                this.LoggedInSubject.next(true);
+                return true;
+            } else {
+                return false;
+            }
+        }).catch(() => false);
+        
+    }
 }
